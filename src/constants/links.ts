327c3da054cf58513169ee5e3435246da3b1e806
= <hannes.vinzens@fau.de>
2025-09-20 14:19:53 +0200

    links.ts file added

---
+++ src/constants/links.ts
@@ -0,0 +1,8 @@
+import { Href } from "expo-router";
+
+// we make sure, that only valid Links are accepted Here
+export const LINKS: { [key: string]: Href } = {
+  index: "/",
+  profile: "/profile",
+  cars: "/cars",
+};
