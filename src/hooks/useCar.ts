0df2d354f609ce5ad8b1788c78ef853a8008b933
= <hannes.vinzens@fau.de>
2025-09-23 13:18:02 +0200

    added more documentation to useCar.ts

--- src/hooks/useCar.ts
+++ src/hooks/useCar.ts
@@ -1,18 +1,24 @@
 import useSwr from "swr";
 import { getCar } from "../api/services";
 
-const fetcher = (id: string) => {
-  getCar(id);
-  //TODO: error Handling?
+//TODO: derive car class from drizzle and add as : Promise<Car>
+const fetcher = ([, id]: [string, string]) => {
+  getCar(id); //if this function throw an error, it'l be collected by SWR
 };
 
 /**
  *
- * @param id Car id
- * @returns car, isLoading, isError
+ * @param id -> if undesfined, request will pause automatically
+ * @returns {car: Car, isLoading : boolean, isError : any}
+ *
+ * @example
+ * // usage inside a component
+ * const {car, isLoading, isError} = useCar('123ABC')
+ * if (isLoading) return <Spinner />
+ * if (isError) return <Error />
+ * if (!car) return <CarNotFoundError />
  */
-
-const useCar = (id: string) => {
+const useCar = (id: string | undefined) => {
   const { data, error, isLoading } = useSwr(["car", id], fetcher);
   return {
     car: data,
