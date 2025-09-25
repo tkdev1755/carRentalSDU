5e5706c80a13fb01d73dc6a19d41cc06c1710b02
= <hannes.vinzens@fau.de>
2025-09-25 10:01:32 +0200

    small updates

--- src/components/FilterOptions.tsx
+++ src/components/FilterOptions.tsx
@@ -1,10 +1,26 @@
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+/**
+ * This component will set the state in the URL and update it according to user input
+ * @see https://docs.expo.dev/router/reference/url-parameters/
+ */
+
 const FilterOptions = () => {
+  const params = useLocalSearchParams();
   return (
     <View>
       <Text>FilterOptions</Text>
+      <Text
+        onPress={() =>
+          router.setParams({
+            price: 34,
+          })
+        }
+      >
+        Set Price to 34
+      </Text>
     </View>
   );
 };
