bb3891e3c9c3a402a4d6ef3802c4171680e98f32
= <hannes.vinzens@fau.de>
2025-09-25 09:07:40 +0200

    FilterOptions.tsx component created

---
+++ src/components/FilterOptions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const FilterOptions = () => {
+  return (
+    <View>
+      <Text>FilterOptions</Text>
+    </View>
+  );
+};
+
+export default FilterOptions;
+
+const styles = StyleSheet.create({});
