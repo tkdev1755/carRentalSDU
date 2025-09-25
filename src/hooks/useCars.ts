141f162e432a9c2507c61c7981b4c94f92d4c8b4
tkdev1755 <khetibh@gmail.com>
2025-09-25 09:15:21 +0200

    Added CarFilters interface in the useCars.ts file for having filtering options

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -2,13 +2,20 @@ import useSwr from "swr";
 import { getAvailableCars } from "../api/services";
 
 interface CarFilters {
-  //all filter options go here
-  price?: number; // ... and so on
+    //all filter options go here
+    minPrice?: number;
+    maxPrice?: number;
+    type?:string;
+    seats?: number;
+    transmissionType?: string;
+    trunkSpace?: number;
+    engineType?: string;
+    isAvailable?: boolean;
 }
 
 const fetcher = () => getAvailableCars();
 
-const useCars = () => {
+const useCars = (filters:CarFilters) => {
   const { data, error, isLoading } = useSwr("cars", fetcher);
   return {
     cars: data,
