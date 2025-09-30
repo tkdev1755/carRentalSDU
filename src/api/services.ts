5646e4c6734771bb6eb42be573c6075b16cfd03b
Mathias Yde <mathias.yde@hotmail.dk>
2025-09-30 16:56:55 +0200

    Implement mock filtering service for cars

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,12 +1,20 @@
-import { CarFilters } from "../types/CarFilters";
+import { CarFilters } from "@/src/types/CarFilters";
 import { MOCK_CARS } from "./mocks/cars";
 
 export const getFilteredCars = async (filters: CarFilters) => {
-  //TODO: implement me! please! 🫠
+  let candidateCars = [];
 
-  // just simulating some time delay "waiting for the data"
-  await new Promise((res) => setTimeout(res, 500));
-  return MOCK_CARS;
+  for (let car of MOCK_CARS) {
+    if (filters.transmission && filters.transmission !== car.transmission) continue;
+    if (filters.engine && filters.engine !== car.engine) continue;
+    if (filters.availability && filters.availability !== car.availability) continue;
+
+    await new Promise((res) => setTimeout(res, 100));
+    candidateCars.push(car);
+  }
+
+  await new Promise((res) => setTimeout(res, 800));
+  return candidateCars;
 };
 
 export const getCar = async (id: number) => {
