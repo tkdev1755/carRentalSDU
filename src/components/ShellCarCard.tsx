a8bf0ba2c589bf6f914c3bfaa777c6963c309f21
Mathias Yde <mathias.yde@hotmail.dk>
2025-09-30 16:57:11 +0200

    Align ShellCarCard to new data model

--- src/components/ShellCarCard.tsx
+++ src/components/ShellCarCard.tsx
@@ -1,7 +1,6 @@
+import type { Car } from "@/src/types/Car"; // si tu mets ton type à part
 import React from "react";
-import { Image } from "react-native";
-import { Card,Text } from "react-native-paper";
-import type { Car } from "../types/Car"; // si tu mets ton type à part
+import { Card, Text } from "react-native-paper";
 
 type CarCardProps = {
     car: Car;
@@ -13,10 +12,10 @@ export const ShellCarCard: React.FC<CarCardProps> = ({ car }) => {
             <Card.Cover source={{ uri: car.image }} />
             <Card.Content>
                 <Text variant={"titleMedium"}>{car.name}</Text>
-                <Text variant={"bodyMedium"}>{car.type} • {car.seats} seats</Text>
-                <Text variant={"bodyMedium"}>{car.transmission_type} • {car.engine_type}</Text>
+                <Text variant={"bodyMedium"}>{car.seats} seats</Text>
+                <Text variant={"bodyMedium"}>{car.transmission} • {car.engine}</Text>
                 <Text variant={"bodyMedium"}>{car.price.toFixed(2)} € / day</Text>
-                {!car.is_available && (
+                {car.availability === "unavailable" && (
                     <Text variant={"bodyMedium"} style={{ color: "red" }}>Not available</Text>
                 )}
             </Card.Content>
