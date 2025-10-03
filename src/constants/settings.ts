df6788ed5952975f62bb488a256e1ba87c69a012
= <hannes.vinzens@fau.de>
2025-10-03 14:54:23 +0200

    removed type specification to keep it simple

---
+++ src/constants/settings.ts
@@ -0,0 +1,11 @@
+/**
+ * this file contains general settings for the app
+ * (not user settings)!!!
+ */
+
+export const SETTINGS = {
+  SNACKBAR: {
+    DISMISS_TEXT: "Okay",
+    CLOSING_DELAY: 7000,
+  },
+};
