f623c2cfe1bc6e09decce28342cdd3da0f807d96
= <hannes.vinzens@fau.de>
2025-10-04 14:09:22 +0200

    ported page to react native paper

--- src/app/cars/car.tsx
+++ src/app/cars/car.tsx
@@ -3,38 +3,49 @@ import { useCar } from "@/src/hooks/useCar";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { useLocalSearchParams } from "expo-router";
 
-import { Image, Pressable, StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
-
+import { StyleSheet, View } from "react-native";
+import { Button, Card, Text, useTheme } from "react-native-paper";
 
 export default function Car() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { car, isLoading, error } = useCar(Number(id));
 
-  if (isLoading)  return (<Text>Loading...</Text>)
-  if (error) return (<Text>Error: {error.message}</Text>)
+  const theme = useTheme();
 
-  const VerticalSeperator = () => <View style={{ width: 1, backgroundColor: '#ccc', marginHorizontal: 8 }} />;
-  
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error: {error.message}</Text>;
+
+  const VerticalSeperator = () => (
+    <View
+      style={{
+        width: 1,
+        backgroundColor: theme.colors.secondary,
+        marginHorizontal: 8,
+      }}
+    />
+  );
+
   const FeatureIcon = (text: string, icon: IconName) => (
-    <View style={{ flexDirection: 'column', alignItems: 'center', gap: 4 }}>
-      <MaterialCommunityIcons name={icon} color="#666" size={24} />
-      <Text>{text}</Text>
+    <View style={{ flexDirection: "column", alignItems: "center", gap: 4 }}>
+      <MaterialCommunityIcons
+        name={icon}
+        color={theme.colors.secondary}
+        size={24}
+      />
+      <Text variant="labelSmall">{text}</Text>
     </View>
   );
 
-  const handleNext = () => {
-    // TODO(mathias): handle the next button press
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.details}>
-      <Image source={{ uri: car?.image }} style={styles.image} />
-        <View>
-          <Text style={styles.title}>{car?.name}</Text>
-          <Text style={styles.description}>{car?.type}</Text>
-        </View>
+    <Card style={{ ...styles.container }}>
+      <Card.Cover source={{ uri: car?.image }} style={styles.image} />
+      <Card.Title
+        title={car?.name}
+        subtitle={car?.type}
+        titleVariant="displayMedium"
+        subtitleVariant="labelMedium"
+      />
+      <Card.Content>
         <View style={[styles.detail, styles.summary]}>
           {FeatureIcon(`${car?.seats} seats`, ICONS.SEAT)}
           <VerticalSeperator />
@@ -42,26 +53,31 @@ export default function Car() {
           <VerticalSeperator />
           {FeatureIcon(`${5} doors`, ICONS.DOOR)}
           <VerticalSeperator />
-          {FeatureIcon(car?.transmission_type || "Auto", ICONS.TRANSMISSION)}
+          {FeatureIcon(car?.transmission || "Auto", ICONS.TRANSMISSION)}
         </View>
+
         <View style={{ marginTop: 4, padding: 8 }}>
-          <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 16, gap: 8 }}>
+          <View style={styles.rowDetails}>
             <Text>Model</Text>
             <Text>Miles: 20,000</Text>
           </View>
-          <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 16, gap: 8 }}>
+          <View style={styles.rowDetails}>
             <Text>Color: White</Text>
             <Text>{car?.is_available ? "Available" : "Not Available"}</Text>
           </View>
-          <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginTop: 16, gap: 8 }}>
-            <Text>EUR {car?.price} / day</Text>
-            <Pressable style={styles.button} onPress={handleNext}>
-              <Text style={{textAlign: "center"}}>Next</Text>
-            </Pressable>
+          <View style={styles.rowDetails}>
+            <Text variant="titleMedium" style={{ color: theme.colors.primary }}>
+              {car?.price}€ / day
+            </Text>
           </View>
         </View>
-      </View>
-    </View>
+      </Card.Content>
+      <Card.Actions>
+        <Button mode="contained-tonal" onPress={() => {}}>
+          Book Car
+        </Button>
+      </Card.Actions>
+    </Card>
   );
 }
 
@@ -79,39 +95,28 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     marginHorizontal: 16,
   },
-  button: {
-    backgroundColor: "lightblue",
-    padding: 8,
-    borderRadius: 4,
-    width: 100,
-    textAlign: "center",
-  },
   image: {
     marginHorizontal: "auto",
     borderRadius: 2,
     marginBottom: 8,
     aspectRatio: "16/9",
   },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 4,
-  },
-  description: {
-    fontSize: 14,
-    color: "#666",
-  },
   detail: {
     flexDirection: "row",
     justifyContent: "space-between",
     marginTop: 16,
-    gap: 8
+    gap: 8,
   },
   details: {
     padding: 12,
     borderWidth: 0.5,
     backgroundColor: "white",
-    borderColor: "#ccc"
-  }
+    borderColor: "red",
+  },
+  rowDetails: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 16,
+    gap: 8,
+  },
 });
-
