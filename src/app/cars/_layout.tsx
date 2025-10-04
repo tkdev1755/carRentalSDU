7b85321094856d969c4e886f235650665b3c5a0f
= <hannes.vinzens@fau.de>
2025-10-04 14:10:39 +0200

    added screenLayout to Tabs and Stack Navigator

--- src/app/cars/_layout.tsx
+++ src/app/cars/_layout.tsx
@@ -1,5 +1,7 @@
 import CustomAppbar from "@/src/components/CustomAppbar";
 import { Stack } from "expo-router";
+import { View } from "react-native";
+import { useTheme } from "react-native-paper";
 
 /**
  * Component for displaying a stack inside the tab layout.
@@ -7,15 +9,24 @@ import { Stack } from "expo-router";
  * @returns <Stack>
  */
 export default function Layout() {
+  const theme = useTheme();
   return (
     <Stack
       screenOptions={{
         header: (props) => <CustomAppbar {...props} />,
       }}
+      screenLayout={({ children }) => (
+        <View style={{ flex: 1, backgroundColor: theme.colors.background }}>
+          {children}
+        </View>
+      )}
     >
       <Stack.Screen name="index" options={{ title: "CarList" }} />
       <Stack.Screen name="car" options={{ title: "" }} />
-      <Stack.Screen name="BookingPage" options={{ title: "Create reservation" }} />
+      <Stack.Screen
+        name="BookingPage"
+        options={{ title: "Create reservation" }}
+      />
       {/* screen options are set dynamicaly in car.tsx */}
     </Stack>
   );
