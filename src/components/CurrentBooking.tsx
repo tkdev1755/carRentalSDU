891b5eeff0e4e4c5d4030ccc6eadf07f93b017ec
= <hannes.vinzens@fau.de>
2025-10-05 21:31:05 +0200

    ported hooks to swr

--- src/components/CurrentBooking.tsx
+++ src/components/CurrentBooking.tsx
@@ -1,53 +1,53 @@
 import * as React from "react";
-import { FlatList, View,StyleSheet } from 'react-native';
-import {Text, ActivityIndicator} from 'react-native-paper';
+import { FlatList, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Text } from "react-native-paper";
+import { BookingType } from "../database/schema";
 import BookingItem from "./BookingItem";
-import {Booking} from "../hooks/useCurrentBookings";
 
-type Props = {bookings:Booking[]; loading:boolean};
+type Props = { bookings: BookingType[] | undefined; loading: boolean };
 
-export default function CurrentBooking({bookings,loading}:Props){
-    if(loading){
-        return (
-            <View style={styles.center}>
-                <ActivityIndicator animating={true} size={"large"}/>
-            </View>
-        );
-    }
-
-    if(!loading && bookings.length===0) {
-        return(
-            <View style={styles.center}>
-                <Text style={styles.noBookingsText}>No current bookings</Text>
-            </View>
-        );
-    }
-
+export default function CurrentBooking({ bookings, loading }: Props) {
+  if (loading) {
     return (
-        <FlatList
-            data={bookings}
-            keyExtractor={(item)=>item.id.toString()}
-            renderItem={({item}) => <BookingItem booking={item}/>}
-            contentContainerStyle={styles.list}
-            scrollEnabled={false}
-            showsVerticalScrollIndicator={false}
-        />
+      <View style={styles.center}>
+        <ActivityIndicator animating={true} size={"large"} />
+      </View>
     );
+  }
+
+  if (!loading && bookings && bookings.length === 0) {
+    return (
+      <View style={styles.center}>
+        <Text style={styles.noBookingsText}>No current bookings</Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={bookings}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => <BookingItem booking={item} />}
+      contentContainerStyle={styles.list}
+      scrollEnabled={false}
+      showsVerticalScrollIndicator={false}
+    />
+  );
 }
 
-const styles=StyleSheet.create({
-    list:{
-        paddingHorizontal:16,
-        paddingBottom:20,
-    },
-    center:{
-        flex:1,
-        justifyContent:'flex-start',
-        alignItems:"center",
-        paddingTop:10,
-    },
-    noBookingsText: {
-        fontSize:18,
-        textAlign: "center",
-    },
+const styles = StyleSheet.create({
+  list: {
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+  },
+  center: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 10,
+  },
+  noBookingsText: {
+    fontSize: 18,
+    textAlign: "center",
+  },
 });
